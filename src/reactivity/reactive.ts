66c58f2d4fd35d8f6fede4f2dc84c4c631c3d48a
iamzjt-front-end <iamzjt@foxmail.com>
2022-06-10 00:30:36 +0800

    ..

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,11 @@
 export function reactive(raw) {
   return new Proxy(raw, {
-    
-  })
+    get(target, key) {
+      // target 目标对象 { foo: 1 }  key 属性 foo
+      const res = Reflect.get(target, key);
+
+      // TODO 依赖收集
+      return res;
+    },
+  });
 }
