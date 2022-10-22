b60ad0906e4013393a65bdc6d02c731686f5c7b2
iamzjt-front-end <iamzjt@foxmail.com>
2022-10-22 23:55:50 +0800

    :fire: feat: compiler-sfc体验 App.vue

---
+++ src/examples/rollup-vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    IamZJT
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+  }
+};
+</script>
+
+<style scoped>
+.app {
+  color: black;
+}
+</style>
