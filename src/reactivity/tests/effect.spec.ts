46c4b1e58010a16480f85f15d49ade90aa1a5aa1
iamzjt-front-end <iamzjt@foxmail.com>
2022-11-04 08:27:37 +0800

    :rocket: feat: reactivity happy path 单测

---
+++ src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,21 @@
+describe('effect', function () {
+  it.skip('happy path', function () {
+    // * 首先定义一个响应式对象
+    const user = reactive({
+      age: 10,
+    });
+
+    // * get -> 收集依赖
+    let nextAge;
+    effect(() => {
+      nextAge = user.age + 1;
+    });
+
+    // * effect默认会执行一次
+    expect(nextAge).toBe(11);
+
+    // * set -> 触发依赖
+    user.age++;
+    expect(nextAge).toBe(12);
+  });
+});
--- src/reactivity/tests/index.spec.ts
+++
@@ -1,5 +0,0 @@
-import { add } from '../index';
-
-it('init', function () {
-  expect(add(1, 1)).toBe(2);
-});
