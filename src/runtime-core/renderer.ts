4846dab332fb5402f17e0e694ba1198e4060e5a9
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-08 14:35:49 +0800

    :truck: feat: 实现组件代理对象

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -53,7 +53,8 @@ function mountComponent(vnode, container) {
 }
 
 function setupRenderEffect(instance, container) {
-  const subTree = instance.render();
+  const { proxy } = instance;
+  const subTree = instance.render.call(proxy);
 
   // vnode -> patch
   // vnode -> element -> mountElement
