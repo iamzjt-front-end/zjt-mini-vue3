55fb386dd20138767aa727b773ca3d0477d44b41
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-08 13:38:06 +0800

    :truck: feat: 删除多余注释

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -7,11 +7,6 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  // 处理组件
-  // todo 判断 是不是 element
-  // 是 element 的话，那么就应该处理 element
-  // 如何区分 element 还是 component 类型？ -> type
-
   if (typeof vnode.type === 'string') {
     processElement(vnode, container);
   } else if (isObject(vnode.type)) {
