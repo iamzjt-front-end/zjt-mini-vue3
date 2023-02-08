22e78f6d55c07241641ec4468641361849a036e4
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-08 16:15:33 +0800

    :truck: feat: 实现this.$el，this挂载组件根节点

--- examples/hello-world/App.js
+++ examples/hello-world/App.js
@@ -1,14 +1,19 @@
 import { h } from '../../lib/zjt-mini-vue.esm.js';
 
+window.self = null;
+
 export const App = {
   // .vue
   // template
   // render
   render() {
     // ui
+    window.self = this;
     return h(
       'div',
       { id: 'root', class: ['red', 'active'] },
+      // setupState
+      // this.$el -> get root element
       'hi, ' + this.msg
       // 'hi, mini-vue'
       // [
