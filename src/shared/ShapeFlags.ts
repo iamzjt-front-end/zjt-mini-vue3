3adb56126835d6c4dc12f992a5906d2d4bc9bc6f
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-09 06:48:02 +0800

    :truck: feat: enum comments

--- src/shared/ShapeFlags.ts
+++ src/shared/ShapeFlags.ts
@@ -1,6 +1,6 @@
 export const enum ShapeFlags {
-  ELEMENT = 1, // 0001
-  STATEFUL_COMPONENT = 1 << 1, // 0010
-  TEXT_CHILDREN = 1 << 2, // 0100
-  ARRAY_CHILDREN = 1 << 3, // 1000
+  ELEMENT = 1, // 0001 -> HTML 或 SVG 标签 普通 DOM 元素
+  STATEFUL_COMPONENT = 1 << 1, // 0010 -> 普通有状态组件
+  TEXT_CHILDREN = 1 << 2, // 0100 -> 子节点是纯文本
+  ARRAY_CHILDREN = 1 << 3, // 1000 -> 子节点是数组
 }
