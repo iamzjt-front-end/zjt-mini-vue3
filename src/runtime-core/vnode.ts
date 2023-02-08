d6faa4dcfaf419b81d5213118a0aa85f99d1f7ae
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-09 06:31:28 +0800

    :truck: feat: 给vnode加上ShapeFlag标识

--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,10 +1,25 @@
+import { ShapeFlags } from '../shared/ShapeFlags';
+import { isArray } from '../shared';
+
 export function createVNode(type, props?, children?) {
   const vnode = {
     type,
     props,
     children,
+    shapeFlag: getShapeFlag(type),
     el: undefined
   };
 
+  // 判断子节点类型并加上
+  if (typeof vnode.children === 'string') {
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN;
+  } else if (isArray(vnode.children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
+  }
+
   return vnode;
+}
+
+function getShapeFlag(type) {
+  return typeof type === 'string' ? ShapeFlags.ELEMENT : ShapeFlags.STATEFUL_COMPONENT;
 }
