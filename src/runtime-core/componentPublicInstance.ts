6404474e7af8f7cf0bb2d9f047c839cececb1b56
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-09 15:35:57 +0800

    :truck: feat: 实现组件的props

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -5,9 +5,17 @@ const publicPropertiesMap = {
 export const PublicInstanceHandlers = {
   get({ _: instance }, key) {
     // setupState: setup return出来的
-    const { setupState } = instance;
+    const { setupState, props } = instance;
     if (key in setupState) {
       return setupState[key];
+    }
+
+    const hasOwn = (val, key) => Object.prototype.hasOwnProperty.call(val, key);
+
+    if (hasOwn(setupState, key)) {
+      return setupState[key];
+    } else if (hasOwn(props, key)) {
+      return props[key];
     }
 
     const publicGetter = publicPropertiesMap[key];
