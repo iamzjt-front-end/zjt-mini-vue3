20be1face9456e55a18596206ed067273158912e
iamzjt-front-end <iamzjt@foxmail.com>
2023-02-21 07:32:16 +0800

    :alembic: feat: 实现自定义渲染器对其他平台API（canvas平台API）的支持

--- examples/customRenderer/main.js
+++ examples/customRenderer/main.js
@@ -1,5 +1,5 @@
-// import { createRenderer } from '../../lib/zjt-mini-vue.esm.js';
-// import { App } from './App.js';
+import { createRenderer } from '../../lib/zjt-mini-vue.esm.js';
+import { App } from './App.js';
 
 console.log(PIXI);
 
@@ -10,18 +10,23 @@ const game = new PIXI.Application({
 
 document.body.append(game.view);
 
-// const renderer = createRenderer({
-// 	createElement(type) {
-//
-// 	},
-// 	patchProp(el, key, value) {
-//
-// 	},
-// 	insert(el, parent) {
-//
-// 	}
-// });
-//
-// const rootContainer = document.querySelector('#app');
-//
-// renderer.createApp(App).mount(rootContainer);
+const renderer = createRenderer({
+	createElement(type) {
+		if (type === 'rect') {
+			const rect = new PIXI.Graphics();
+			rect.beginFill(0xff0000);
+			rect.drawRect(0, 0, 100, 100);
+			rect.endFill();
+
+			return rect;
+		}
+	},
+	patchProp(el, key, val) {
+		el[key] = val;
+	},
+	insert(el, parent) {
+		parent.addChild(el);
+	}
+});
+
+renderer.createApp(App).mount(game.stage);
