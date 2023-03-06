c4c04d5f03dbfcd627bd5e0e7e8af29f43573d42
iamzjt-front-end <iamzjt@foxmail.com>
2023-03-06 15:58:00 +0800

    :egg: feat: 创建新的节点

--- examples/patchChildren/ArrayToArray.js
+++ examples/patchChildren/ArrayToArray.js
@@ -144,11 +144,33 @@ import { ref, h } from '../../lib/zjt-mini-vue.esm.js';
 // * a, b, (c, d, e), f, g
 // * a, b, (e, c, d), f, g
 // * 最长子序列: [1, 2]
+// const prevChildren = [
+// 	h('p', { key: 'A' }, 'A'),
+// 	h('p', { key: 'B' }, 'B'),
+// 	h('p', { key: 'C' }, 'C'),
+// 	h('p', { key: 'D' }, 'D'),
+// 	h('p', { key: 'E' }, 'E'),
+// 	h('p', { key: 'F' }, 'F'),
+// 	h('p', { key: 'G' }, 'G'),
+// ];
+// const nextChildren = [
+// 	h('p', { key: 'A' }, 'A'),
+// 	h('p', { key: 'B' }, 'B'),
+// 	h('p', { key: 'E' }, 'E'),
+// 	h('p', { key: 'C' }, 'C'),
+// 	h('p', { key: 'D' }, 'D'),
+// 	h('p', { key: 'F' }, 'F'),
+// 	h('p', { key: 'G' }, 'G'),
+// ];
+
+// * 5.3 创建新的节点
+// * a, b, (c, e), f, g
+// * a, b, (e, c, d), f, g
+// * d 节点在老的节点中不存在，新的里面存在，所以需要创建
 const prevChildren = [
 	h('p', { key: 'A' }, 'A'),
 	h('p', { key: 'B' }, 'B'),
 	h('p', { key: 'C' }, 'C'),
-	h('p', { key: 'D' }, 'D'),
 	h('p', { key: 'E' }, 'E'),
 	h('p', { key: 'F' }, 'F'),
 	h('p', { key: 'G' }, 'G'),
