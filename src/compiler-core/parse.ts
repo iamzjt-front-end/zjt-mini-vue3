5f33838675bac4e6d91ac535036389fcbd5c2b9e
iamzjt-front-end <iamzjt@foxmail.com>
2023-03-20 07:23:08 +0800

    :fire: feat: 实现插值element

--- src/compiler-core/parse.ts
+++ src/compiler-core/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast';
 
+const enum TagType {
+	start,
+	end
+}
+
 export function baseParse(content: string) {
 	const context = createParseContext(content);
 
@@ -16,12 +21,40 @@ function parseChildren(context) {
 	const nodes: any[] = [];
 
 	let node;
-	if (context.source.startsWith('{{')) {
+	const s = context.source;
+	if (s.startsWith('{{')) {
 		node = parseInterpolation(context);
+	} else if (s[0] === '<') {
+		if (/[a-z]/i.test(s[1])) {
+			node = parseElement(context);
+		}
 	}
 
 	nodes.push(node);
 	return nodes;
+}
+
+function parseElement(context) {
+	const element = parseTag(context, TagType.start);
+	parseTag(context, TagType.end);
+
+	return element;
+}
+
+function parseTag(context, type: TagType) {
+	// 1. 解析tag
+	const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+	const tag = match[1];
+	// 2. 删除处理完成的代码
+	advanceBy(context, match[0].length);
+	advanceBy(context, 1);
+
+	if (type === TagType.end) return;
+
+	return {
+		type: NodeTypes.ELEMENT,
+		tag
+	};
 }
 
 function parseInterpolation(context) {
