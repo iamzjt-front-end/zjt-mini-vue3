59197f77f844c476cc216a6f44ce3854a559017c
iamzjt-front-end <iamzjt@foxmail.com>
2023-03-20 11:49:42 +0800

    :fire: refactor: 不小心删除测试用例，补充进来

--- src/compiler-core/__tests__/parse.spec.ts
+++ src/compiler-core/__tests__/parse.spec.ts
@@ -18,6 +18,17 @@ describe('Parse', () => {
 		});
 	});
 
+	describe('element', () => {
+		it('simple element div', () => {
+			const ast = baseParse('<div></div>');
+
+			expect(ast.children[0]).toStrictEqual({
+				type: NodeTypes.ELEMENT,
+				tag: 'div'
+			});
+		});
+	});
+
 	describe('text', () => {
 		it('simple text', () => {
 			const ast = baseParse('some text');
@@ -26,6 +37,6 @@ describe('Parse', () => {
 				type: NodeTypes.TEXT,
 				content: 'some text'
 			});
-		})
-	})
+		});
+	});
 });
