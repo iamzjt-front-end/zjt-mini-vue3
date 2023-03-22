9e8e3f2f7fea2f80b44451b8ac3676ca9cfa6e92
iamzjt-front-end <iamzjt@foxmail.com>
2023-03-22 13:41:10 +0800

    :fire: feat: add nested element case

--- src/compiler-core/__tests__/parse.spec.ts
+++ src/compiler-core/__tests__/parse.spec.ts
@@ -116,5 +116,33 @@ describe('Parse', () => {
 				]
 			});
 		});
+
+		it('nested element', () => {
+			const ast = baseParse('<div><p>hi</p>{{ message }}</div>');
+
+			expect(ast.children[0]).toStrictEqual({
+				type: NodeTypes.ELEMENT,
+				tag: 'div',
+				children: [
+					{
+						type: NodeTypes.ELEMENT,
+						tag: 'p',
+						children: [
+							{
+								type: NodeTypes.TEXT,
+								content: 'hi'
+							}
+						]
+					},
+					{
+						type: NodeTypes.INTERPOLATION,
+						content: {
+							type: NodeTypes.SIMPLE_EXPRESSION,
+							content: 'name'
+						}
+					}
+				]
+			});
+		});
 	});
 });
