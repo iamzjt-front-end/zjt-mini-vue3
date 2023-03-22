01dc22d82f29659c4705da48476e765dda197449
iamzjt-front-end <iamzjt@foxmail.com>
2023-03-22 08:19:52 +0800

    :fire: feat: 完善element测试用例

--- src/compiler-core/__tests__/parse.spec.ts
+++ src/compiler-core/__tests__/parse.spec.ts
@@ -18,13 +18,28 @@ describe('Parse', () => {
 	});
 
 	describe('element', () => {
-		it('simple element div', () => {
+		it('empty', () => {
 			const ast = baseParse('<div></div>');
 
 			expect(ast.children[0]).toStrictEqual({
 				type: NodeTypes.ELEMENT,
 				tag: 'div',
 				children: []
+			});
+		});
+
+		it('simple div', () => {
+			const ast = baseParse('<div>hello</div>');
+
+			expect(ast.children[0]).toStrictEqual({
+				type: NodeTypes.ELEMENT,
+				tag: 'div',
+				children: [
+					{
+						type: NodeTypes.TEXT,
+						content: 'hello'
+					}
+				]
 			});
 		});
 	});
