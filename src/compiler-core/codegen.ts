bb6c8c28a9aed25f29ae84fddc464bf6321d4cdd
iamzjt-front-end <iamzjt@foxmail.com>
2023-03-23 17:28:54 +0800

    🔧 style: code format

--- src/compiler-core/codegen.ts
+++ src/compiler-core/codegen.ts
@@ -4,7 +4,7 @@ function createCodegenContext() {
 		push(source) {
 			context.code += source;
 		}
-	}
+	};
 
 	return context;
 }
@@ -13,14 +13,14 @@ export function generate(ast) {
 	const context = createCodegenContext();
 	const { push } = context;
 
-	push('return ')
+	push('return ');
 
 	const functionName = 'render';
 	const args = ['_ctx', '_cache'];
 	const signature = args.join(', ');
 
 	push(`function ${ functionName } (${ signature }) {`);
-	push('return ')
+	push('return ');
 
 	genCode(ast.codegenNode, context);
 	push(`}`);
